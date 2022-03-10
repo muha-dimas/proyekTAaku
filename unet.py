0290fc54afd740b06dd6374190d83a3f14d85828
Muhammad Dimas <80323200+muha-dimas@users.noreply.github.com>
2022-03-10 14:43:08 +0700

    Update unet.py

--- unet.py
+++ unet.py
@@ -269,10 +269,10 @@ class myUnet(object):
         model.load_weights('./unet_model.hdf5')
         
     def jacard_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+        intersection = K.sum(y_true_f * y_pred_f)
+        return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
 
 
     def jacard_coef_loss(y_true, y_pred):
